05dc0243d4dd810c9b13f5694d6cc2c7ac4ae308
Colin McBride <11056834@live.mercer.edu>
2025-04-18 10:01:19 -0400

    Cleaned up node_modules and fixed git issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // GitHub Pages expects an index.html in the root directory
-  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
-  // needed for github pages just put the repo name here
-  base: '/FallingStones/', 
+  base: '/FallingStones/', // <- Use your GitHub repo name here
 });
